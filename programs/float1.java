d8c341599f446e6a9b7a5aae028871e3269d07de
Utkarsh Sinha <gitboyrex@gmail.com>
2024-10-13 12:11:48 +0530

    files modified

--- programs/float1.java
+++ programs/float1.java
@@ -1,6 +1,6 @@
 public class float1{
 	public static void main(String[] args){
-		float a=100.0f;
+		float a=100.03f;
 		System.out.println(a);
 	}
 }
---
+++ programs/operator.java
@@ -0,0 +1,7 @@
+public class operator{
+	public static void main(String[] args){
+		int a = 100;
+		int b = 20;
+		System.out.println(a*b);
+	}
+}
